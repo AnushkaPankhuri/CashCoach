81290ca45c44e24cef638789df8e7a6be3397ea7
AnushkaPankhuri <anushka.pat.bih@gmail.com>
2025-03-29 19:21:10 +0530

    fixed the connection to pathway by updating the functions and model

--- finance-tracker/backend/tracker/urls.py
+++ finance-tracker/backend/tracker/urls.py
@@ -3,14 +3,14 @@ from django.urls import path
 from .views import UploadCSV
 from . import views
 
+from django.urls import path
+from django.contrib import admin
+from . import views
+
 urlpatterns = [
+   
     path('admin/', admin.site.urls),
     path('', views.dashboard, name='dashboard'),
-    path('upload/', UploadCSV.as_view(), name='upload_csv'),
-    # path('upload/', views.UploadCSV, name='upload_csv'),
-    path('transactions/', views.all_transactions, name='all_transactions'),
-    path('transactions/filter/', views.filter_transactions, name='filter_transactions'),
-    path('transactions/summary/', views.summary, name='summary'),
-    path('api/get-insights/', views.get_insights, name='get_insights'),
-    path('set_budget/', views.set_budget_threshold, name='set_budget')
+    path('upload/', views.UploadCSVView.as_view(), name='upload_csv'),
+    path('set-budget/', views.BudgetView.as_view(), name='set_budget'),
 ]
